7c21a4fb929f1b43e3b768d3bb04e74f0a52643e
Schwenk-A <100094451+Schwenk-A@users.noreply.github.com>
2022-09-06 20:56:02 -0500

    Created Template

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
