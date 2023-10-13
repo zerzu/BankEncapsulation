ad4d17ddfae99b3024fe97a1a775b44ee77aad93
Eric Garner <ericgarner81@gmail.com>
2023-10-13 09:10:42 -0500

    first commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private decimal balance = 0.00m;
+
+        public static void MainMenu(BankAccount account, bool exit)
+        {
+            while (exit == true)
+            {
+                Console.WriteLine(@"______             _    _                ___              ");
+                Console.WriteLine(@"| ___ \           | |  (_)              / _ \ ");
+                Console.WriteLine(@"| |_/ / __ _ _ __ | | ___ _ __   __ _  / /_\ \_ __  _ __ ");
+                Console.WriteLine(@"| ___ \/ _` | '_ \| |/ / | '_ \ / _` | |  _  | '_ \| '_ \");
+                Console.WriteLine(@"| |_/ / (_| | | | |   <| | | | | (_| | | | | | |_) | |_) |");
+                Console.WriteLine(@"\____/ \__,_|_| |_|_|\_\_|_| |_|\__, | \_| |_/ .__/| .__/ ");
+                Console.WriteLine(@"                                 __/ |       | |   | |  ");
+                Console.WriteLine(@"                                |___/        |_|   |_|    ");
+                Console.WriteLine(@"---------------------------------------------------------");
+                Console.WriteLine("");
+
+                bool isNumber;
+                int menuOption;
+
+
+                do
+                {
+
+                    Console.WriteLine("What brings you in today");
+                    Console.WriteLine(@"1) Deposit");
+                    Console.WriteLine(@"2) Check Balance");
+                    Console.WriteLine(@"3) Exit");
+                    Console.WriteLine(@"-----------------------");
+                    Console.Write("Selection:");
+
+                    isNumber = int.TryParse(Console.ReadLine(), out menuOption);
+
+                } while (!isNumber);
+
+                switch (menuOption)
+                {
+                    case 1:
+                        account.DepositAmount();
+                        break;
+                    case 2:
+                        account.GetBalance();
+                        break;
+                    default:
+                        Console.WriteLine("Goodbye!");
+
+                        exit = false;
+                        break;
+                }
+            }
+        }
+
+        public void DepositAmount()
+        {
+            decimal dollarAmount;
+            bool isDollar;
+
+            do
+            {
+                Console.WriteLine("How much would you like to deposit?");
+
+                isDollar = (decimal.TryParse(Console.ReadLine(), out dollarAmount));
+
+            } while (!isDollar);
+
+            Deposit(dollarAmount);
+            Console.WriteLine($"Deposited {dollarAmount} into your account.");          
+            
+            
+        }
+
+        void Deposit(decimal depositAmount)
+        {
+            balance += depositAmount;
+        }
+
+        decimal GetBalance()
+        {
+            Console.WriteLine($"{balance}");
+
+            return balance;
+        }
+
+         
+    }
+}
+
+
+
+
+        
+    
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace BankEncapsulation
+﻿using static System.Formats.Asn1.AsnWriter;
+using System;
+using System.Security.Principal;
+
+namespace BankEncapsulation
 {
     public class Program
     {
+        //Create a new public class named BankAccount           DONE!
+        //In your BankAccount class, create the following:
+        //A private field of type double named **balance** with a value of 0    DONE!!
+        //Define a method named Deposit that will accept a double and store that value in the balance field DONE!!
+        //Define a method named GetBalance that will return the amount stored in the balance field  DONE!!
+        //In the main method of your application, create a new instance of the BankAccount class.   DONE!!
+        //Allow the user of the application to Deposit money and retrieve their balance through the console.
+        //Once finished save, commit, and push back to Github
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            BankAccount account = new BankAccount();
+
+
+            BankAccount.MainMenu(account, true);
+
+
+        }        
+       
+            
+            
+        
     }
 }
